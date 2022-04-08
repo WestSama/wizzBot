2ba317a07618a4cdfad62893938302b2f83052df
WestSama <leetlordpt@gmail.com>
2022-04-08 17:20:25 +0100

    Added cogs, new file structure and slash commands

---
+++ cogs/Greetings.py
@@ -0,0 +1,66 @@
+import discord
+import random
+from discord.ext import commands
+
+
+class Greetings(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        print(message.content)
+        msgs = ["Don't ping me you blob!",
+                "I will kill you if you ping me!",
+                "I dare you to ping me again!",
+                "Who pinged me! REEEE",
+                "Poke, poke, poke is that all you do?"
+        ]
+        msgBot = random.choice(msgs)
+        if message.content.startswith("<@760636930265317450>"):
+            await message.channel.send(msgBot)
+    
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        channel = self.client.get_channel(520931518655234048)
+        await channel.send(f"{member} -- {member.display_name} has join the server!")
+
+
+    @commands.Cog.listener()
+    async def on_member_remove(self, member):
+        channel = self.client.get_channel(520931518655234048)
+        await channel.send(f"{member} -- {member.display_name} has left the server!")
+
+    @commands.Cog.listener()
+    async def on_member_update(self, before, after):
+        channel = self.client.get_channel(520931518655234048)
+        if before.nick != after.nick:
+
+            await channel.send(f"{before.name} changed his/her name to {after.nick}")
+
+    @commands.slash_command(description="Is this user sus??")
+    async def sus(self, ctx, member: discord.Member):
+        susMsg = [
+                f"{member.name} is sus!",
+                f"{member.name} isn't sus."
+        ]
+        susSend = random.choice(susMsg)
+        await ctx.respond(susSend)
+
+    @commands.slash_command(description="Bot will greet you with a msg.")
+    async def hello(self, ctx):
+        helloMsgs = [f"{ctx.author.mention} hello you blob!",
+                    f"{ctx.author.mention} hello only to you wizzel.",
+                    f"{ctx.author.mention} no hello for you today."
+        ]
+        msg = random.choice(helloMsgs)
+        await ctx.respond(msg)
+
+
+    @commands.slash_command(description="Use this command to slap a user.")
+    async def slap(self, ctx, member: discord.Member):
+        print(member)
+        await ctx.respond(f"{ctx.author.name} slapped {member.mention}!!!")
+
+def setup(client):
+    client.add_cog(Greetings(client))
--- wizzBot.py
+++ wizzBot.py
@@ -13,52 +13,29 @@ intents.message_content = True
 
 activity = discord.Game(name="Lost Ark EU")
 
-bot = discord.Bot(debug_guilds=[763654705649156098], intents=discord.Intents.all(), activity=activity)
+bot = commands.Bot(debug_guilds=[763654705649156098, 514264673831616512], intents=discord.Intents.all(), activity=activity, command_prefix="!")
 
 @bot.event
 async def on_ready():
     print(f'{bot.user} is ready and Online!')
 
-@bot.event
-async def on_message(message):
+for file in os.listdir("./cogs"):
+    if file.endswith(".py"):
+        bot.load_extension(f"cogs.{file[:-3]}")
 
-    msgs = ["Don't ping me you blob!",
-            "I will kill you if you ping me!",
-            "I dare you to ping me again!",
-            "Who pinged me! REEEE",
-            "Poke, poke, poke is that all you do?"
-    ]
-    msgBot = random.choice(msgs)
-    if message.content.startswith("<@!760636930265317450>"):
-        await message.channel.send(msgBot)
+@bot.command()
+async def load(ctx, extension):
+    bot.load_extension(f"cogs.{extension}")
+    await ctx.send("Loaded cog!")
 
-@bot.event
-async def on_member_remove(member:discord.User):
-    channel = bot.get_channel(520931518655234048)
-    await channel.send(f"{member.display_name} has left the server!")
+@bot.command()
+async def unload(ctx, extension):
+    bot.unload_extension(f"cogs.{extension}")
+    await ctx.send("Unloaded cog!")
 
-@bot.command(description="Is this user sus??")
-async def sus(ctx, member: discord.Member):
-    susMsg = [
-            f"{member.name} is sus!",
-            f"{member.name} isn't sus."
-    ]
-    susSend = random.choice(susMsg)
-    await ctx.respond(susSend)
-
-@bot.command(description="Bot will greet you with a msg.")
-async def hello(ctx):
-    helloMsgs = [f"{ctx.author.mention} hello you blob!",
-                f"{ctx.author.mention} hello only to you wizzel.",
-                f"{ctx.author.mention} no hello for you today."
-    ]
-    msg = random.choice(helloMsgs)
-    await ctx.respond(msg)
-
-
-@bot.command(description="Use this command to slap a user.")
-async def slap(ctx, member: discord.Member):
-    
-    await ctx.respond(f"{ctx.author.name} slapped {member.mention}!!!")
+@bot.command()
+async def reload(ctx, extension):
+    bot.reload_extension(f"cogs.{extension}")
+    await ctx.send("Reloaded cog!")
 
 bot.run(os.getenv('WIZZBOT_TOKEN'))
